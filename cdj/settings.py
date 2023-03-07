720e16f82ad213b2d2d00aae632080308b3fa017
rosaidabhz <rosaidabhz@gmail.com>
2023-03-07 17:33:44 -0500

    first commit home

--- cdj/settings.py
+++ cdj/settings.py
@@ -116,10 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
-MEDIA_URL = '/public/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = '/static/'
+MEDIA_URL = "/public/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ home/static/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elemssidenav = document.querySelectorAll('.sidenav');
+    var instancessidenav = M.Sidenav.init(elemssidenav);
+   });
+
