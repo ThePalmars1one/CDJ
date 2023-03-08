7fce3c9c57c5ae47bb43606ced1bac8f727f98c1
Andrea Elbittar <andreaelbittarl@gmail.com>
2023-03-08 17:21:19 -0500

    first commit

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
-from home.models import Consejos
+from home.models import Consejos,Aboutus, Banner, Collaborators, Documents, Prueba
 
-admin.site.register(Consejos)
+
+@admin.register(Consejos)
+class ConsejosAdmin(admin.ModelAdmin):
+    list_display = ('name','user','type_consejo')
+
+@admin.register(Aboutus)
+class AboutusAdmin(admin.ModelAdmin):
+    list_display = ('name','description')
+
+
+
+@admin.register(Collaborators)
+class CollaboratorsAdmin(admin.ModelAdmin):
+    list_display = ('name','lastname', 'position')
+
+@admin.register(Documents)
+class DocumentsAdmin(admin.ModelAdmin):
+    list_display = ('title','description','date')
+
+admin.site.register(Banner)
+
--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.fields.files import ImageField
 from django.contrib.auth.models import User
+import datetime
 
 class Consejos(models.Model):
     CONSEJO_CHOICES = [
@@ -17,4 +18,31 @@ class Consejos(models.Model):
     def __str__(self):
         return self.name
 
+class Banner(models.Model):
+    image = ImageField(upload_to="consejos/banners")
 
+class Aboutus(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500, blank=True)
+    image = ImageField(upload_to="consejos/aboutus")
+
+class Collaborators (models.Model):
+    name = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    description = models.TextField(max_length=500, blank=True)
+    image = ImageField(upload_to="consejos/collaborators")
+
+class Documents (models.Model):
+    title = models.CharField(max_length=100)
+    pdf = models.FileField(upload_to="consejos/documents")
+    description = models.TextField(max_length=500, blank=True)
+    date = models.DateTimeField()
+
+    def save (self, *args, **kwargs):
+        if not self .id:
+            self.date = datetime.datetime.now()
+            return super(Documents, self).save(*args, **kwargs)
+        
+class Prueba (models.Model):
+    name = models.CharField(max_length=100)
